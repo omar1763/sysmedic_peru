17f81a7bfd6c9e85efe0292067dc2392978a321e
Erik Zerpa <zerpaerik@gmail.com>
2018-07-01 19:55:27 -0400

    commit inicial

---
+++ app/Http/Controllers/Archivos/PersonalController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers\Archivos;
+
+use App\Personal;
+use Silber\Bouncer\Database\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Archivos\StorePersonalRequest;
+use App\Http\Requests\Archivos\UpdatePersonalRequest;
+
+class PersonalController extends Controller
+{
+    /**
+     * Display a listing of User.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (! Gate::allows('users_manage')) {
+            return abort(401);
+        }
+
+        $personal = Personal::with('roles')->get();
+
+        return view('archivos.personal.index', compact('personal'));
+    }
+
+    /**
+     * Show the form for creating new User.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        if (! Gate::allows('users_manage')) {
+            return abort(401);
+        }
+
+        return view('archivos.personal.create');
+    }
+
+    /**
+     * Store a newly created User in storage.
+     *
+     * @param  \App\Http\Requests\StoreUsersRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePersonalRequest $request)
+    {
+        if (! Gate::allows('users_manage')) {
+            return abort(401);
+        }
+        $personal = Personal::create($request->all());
+
+        return redirect()->route('admin.personal.index');
+    }
+
+
+    /**
+     * Show the form for editing User.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        if (! Gate::allows('users_manage')) {
+            return abort(401);
+        }
+
+        $personal = Personal::findOrFail($id);
+
+        return view('archivos.personal.edit', compact('personal', 'roles'));
+    }
+
+    /**
+     * Update User in storage.
+     *
+     * @param  \App\Http\Requests\UpdateUsersRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePersonalRequest $request, $id)
+    {
+        if (! Gate::allows('users_manage')) {
+            return abort(401);
+        }
+        $personal = Personal::findOrFail($id);
+        $personal->update($request->all());
+       
+        return redirect()->route('admin.personal.index');
+    }
+
+    /**
+     * Remove User from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if (! Gate::allows('users_manage')) {
+            return abort(401);
+        }
+        $personal = Personal::findOrFail($id);
+        $personal->delete();
+
+        return redirect()->route('admin.personal.index');
+    }
+
+    /**
+     * Delete all selected User at once.
+     *
+     * @param Request $request
+     */
+    public function massDestroy(Request $request)
+    {
+        if (! Gate::allows('users_manage')) {
+            return abort(401);
+        }
+        if ($request->input('ids')) {
+            $entries = User::whereIn('id', $request->input('ids'))->get();
+
+            foreach ($entries as $entry) {
+                $entry->delete();
+            }
+        }
+    }
+
+}
---
+++ app/Personal.php
@@ -0,0 +1,41 @@
+<?php
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
+use Hash;
+
+/**
+ * Class Personal
+ *
+ * @package App
+ * @property string $nombres
+ * @property string $apellidos
+ * @property string $dni
+ * @property string $telefono
+ * @property string $direccion
+*/
+class Personal extends Authenticatable
+{
+    use Notifiable;
+    use HasRolesAndAbilities;
+
+    protected $fillable = ['nombres', 'apellidos', 'dni', 'telefono','direccion'];
+    
+    
+    /**
+     * Hash password
+     * @param $input
+     */
+    public function setPasswordAttribute($input)
+    {
+        if ($input)
+            $this->attributes['password'] = app('hash')->needsRehash($input) ? Hash::make($input) : $input;
+    }
+    
+    
+   
+    
+    
+}
