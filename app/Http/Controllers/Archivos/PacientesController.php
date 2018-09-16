133caf2d1ceb777fb89138a154d4d7d4a03199bb
Erik Zerpa <zerpaerik@gmail.com>
2018-09-16 13:20:06 -0400

    paientes histori

--- app/Http/Controllers/Archivos/PacientesController.php
+++ app/Http/Controllers/Archivos/PacientesController.php
@@ -8,6 +8,7 @@ use App\Distrito;
 use App\EdoCivil;
 use App\GradoInstruccion;
 use App\HistoriasClinicas;
+use App\HistoriaPacientes;
 use App\Empresas;
 use App\Locales;
 use DB;
@@ -135,11 +136,10 @@ class PacientesController extends Controller
        $pacientes->id_sucursal     =$usuarioSuc;
        $pacientes->save();
 
-       $historiaclinica=str_pad(($pacientes->id),4, "0", STR_PAD_LEFT);
 
        $historia = new HistoriasClinicas;
        $historia->id_paciente =$pacientes->id;
-       $historia->historia    =$historiaclinica;
+       $historia->historia    =HistoriaPacientes::generarHistoria($usuarioEmp,$usuarioSuc);
        $historia->save();
       
 
@@ -183,11 +183,11 @@ class PacientesController extends Controller
        $pacientes->id_sucursal     =$usuarioSuc;
        $pacientes->save();
 
-       $historiaclinica=str_pad(($pacientes->id),4, "0", STR_PAD_LEFT);
+     
 
        $historia = new HistoriasClinicas;
        $historia->id_paciente =$pacientes->id;
-       $historia->historia    =$historiaclinica;
+       $historia->historia    =HistoriaPacientes::generarHistoria($usuarioEmp,$usuarioSuc);
        $historia->save();
       
 
@@ -284,5 +284,57 @@ class PacientesController extends Controller
             }
         }
     }
+
+
+     public static function generarHistoria(){
+        
+         $id_usuario = Auth::id();
+
+         $searchUsuarioID = DB::table('users')
+                    ->select('*')
+                   // ->where('estatus','=','1')
+                    ->where('id','=', $id_usuario)
+                    ->get();
+
+            foreach ($searchUsuarioID as $usuario) {
+                    $usuarioEmp = $usuario->id_empresa;
+                    $usuarioSuc = $usuario->id_sucursal;
+                }
+
+      
+        $searchContador= DB::table('historia_pacientes')
+                    ->select('*')
+                    ->where('id_empresa','=',$usuarioEmp)
+                    ->where('id_sucursal','=', $usuarioSuc)
+                    ->get();
+
+        $contador=1;
+          if(count($searchContador) ==0){
+            $contador=1;
+          
+            $historia = new HistoriaPacientes;
+            $historia->historia=$contador;
+            $historia->id_empresa=$usuarioEmp;
+            $historia->id_sucursal=$usuarioSuc;
+            $historia->save();
+
+          
+        } else {
+         foreach ($searchContador as $correlativo){
+            $contador=$correlativo->historia+1;
+
+         
+            $historia=HistoriaPacientes::findOrFail($correlativo->id);
+            $historia->historia=$contador;
+            $historia->updated_at=date('Y-m-d H:i:s');
+            $historia->update();
+
+        } 
+    }
+
+    return str_pad($contador, 4, "0", STR_PAD_LEFT);
+
+    }
+
 
 }
