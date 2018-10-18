b853cc98fb5722c1dc5c6480c57879217b5f19e7
Erik Zerpa <zerpaerik@gmail.com>
2018-10-17 20:56:28 -0400

    historia en 1 linea

--- resources/views/reportes/historia_pacientes_ver.blade.php
+++ resources/views/reportes/historia_pacientes_ver.blade.php
@@ -10,13 +10,8 @@
 
   	<p style="text-align: left;"><center><STRONG>HISTORÌA MÈDICA</STRONG></center></p>
     <p style="text-align: left;"><center><h1>{{Auth::user()->empresa}}</h1></center></p>
-
-    <br><br>
     <p style="text-align: left;"><strong>Nº DE H.C:</strong>{{ $pac->historia}}</p><p style="text-align: right; margin-right: 190px; margin-top: -30px;"><strong>FECHA Y HORA DE ATENCIÒN:</strong></p>
-
-   <br>
    <p style="text-align: left;"><strong>I. DATOS DE AFILIACIÒN:</strong></p> 
-   <br>
    <p style="text-align: left;"><strong>APELLIDOS Y NOMBRES:</strong>{{ $pac->apellidos}},{{ $pac->nombres}}</p> 
    <p style="text-align: left;"><strong>EDAD:</strong></p> 
    <p style="text-align: right; margin-top: -30px; margin-right: 400px;"><strong>ESTADO CIVIL:</strong>{{ $pac->edocivil}}</p>
@@ -31,15 +26,11 @@
    <p style="text-align: right;margin-top: -30px;margin-right: 200px"><strong>TELEFONO CELULAR:</strong>{{ $pac->telefono}}</p>
    <p style="text-align: left;"><strong>ACOMPAÑANTE:</strong>XXXXX</p> 
    <p style="text-align: right;margin-top: -30px;margin-right: 200px"><strong>PARENTESCO:</strong>XXXXXX</p>
-   <br>
    <p style="text-align: left;"><strong>II. ANTECEDENTES:</strong></p> 
-   <br>
    <p style="text-align: left;"><strong>ANTECEDENTES FAMILIARES:___________________________________________________________________</strong></p> 
    <p style="text-align: left;"><strong>ANTECEDENTES PERSONALES:___________________________________________________________________</strong></p> 
    <p style="text-align: left;"><strong>ANTECEDENTES PATOLÒGICOS:___________________________________________________________________</strong></p> 
-   <br><br>
    <p style="text-align: left;"><strong>III. EXAMENES:</strong></p> 
-   <br>
    <p style="text-align: left;"><strong>MOTIVO DE CONSULTA:______________________________________________________________________</strong></p> 
    <p style="text-align: left;"><strong>TIPO DE ENFERMEDAD ACTUAL:____________________</strong></p> 
    <p style="text-align: right;margin-top: -30px;margin-right: 130px"><strong>EVOL. ENF ACTUAL:</strong>____________________</p>
@@ -54,22 +45,18 @@
   <p style="text-align: right; margin-top: -30px; margin-right: 600px;"><strong>SED:</strong></p> 
   <p style="text-align: right; margin-top: -30px; margin-right: 400px;"><strong>SUEÑO:</strong></p> 
   <p style="text-align: right; margin-top: -30px; margin-right: 200px;"><strong>ESTADO DE ANIMO:</strong></p> 
-  <br>
   <p style="text-align: left;"><strong>IV. EXAMEN FÌSICO GENERAL:</strong></p> 
-  <p>______________________________________________________________________________________________</p>
-  <p>______________________________________________________________________________________________</p>
-  <p>______________________________________________________________________________________________</p>
-  <p>______________________________________________________________________________________________</p>
-  <p>______________________________________________________________________________________________</p>
-  <p>______________________________________________________________________________________________</p>
-
-
-
- 
-
- 
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________<br>
+  _______________________________________________________________________________________________________</p> 
  @endforeach
-
-
   </body>
 </html>
